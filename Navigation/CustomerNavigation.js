a82d8e14097faaa50230435a343592c09b485c33
vidalbaquedano <vidalbaquedano@gmail.com>
2022-07-01 18:04:29 -0600

    Structure

---
+++ Navigation/CustomerNavigation.js
@@ -0,0 +1,3 @@
+import React from "react";
+import InformationScreen from "../Screens/InformationScreen";
+import SettingsScreen from "../Screens/SettingsScreen";
---
+++ Screens/LoginScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import { colors } from '../Constant/Colors';
+
+export default function () {
+  const color = { ...colors };
+  const styles = makeStyle(color);
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const makeStyle = (color) => {
+  return StyleSheet.create({
+    container: {},
+  });
+};
